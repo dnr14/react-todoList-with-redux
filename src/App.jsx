4e51b107788ed085400c004876cdc00f3815f0ff
dnr14 <dnr14@naver.com>
2021-09-23 23:52:35 +0900

    반응형 css 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import TodosContainer from "@containers/TodosContainer";
-import { hot } from "react-hot-loader";
 
 const App = () => {
   return <TodosContainer />;
 };
 
-const Hot = hot(App);
-export default Hot;
+export default App;
